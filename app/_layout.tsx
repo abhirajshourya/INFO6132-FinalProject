d9b1371b89960c10b23acfda6326fb57ef4b14a1
DannyGlade <darshannariya12@gmail.com>
2024-07-24 14:30:08 -0400

    Font error fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout() {
     const colorScheme = useColorScheme()
     const [loaded] = useFonts({
         SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+        InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+    
     })
 
     useEffect(() => {
