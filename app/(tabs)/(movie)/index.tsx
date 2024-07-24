901ba46f5f66067c466d9f1b46c0a8b7e3b1b54d
DannyGlade <darshannariya12@gmail.com>
2024-07-24 14:30:28 -0400

    added search and pagination UI

--- app/(tabs)/(movie)/index.tsx
+++ app/(tabs)/(movie)/index.tsx
@@ -1,9 +1,26 @@
 import ContentTile from '@/components/ContentTile'
+import { Ionicons } from '@expo/vector-icons'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { View, Text, ScrollView, YStack, H1, YGroup, Separator } from 'tamagui'
+import {
+    View,
+    Text,
+    ScrollView,
+    YStack,
+    H1,
+    YGroup,
+    Separator,
+    XStack,
+    Input,
+    Button,
+    XGroup,
+    Spinner,
+} from 'tamagui'
 
 const Index = () => {
+    const [search, setSearch] = useState('')
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
     const [movies, setMovies] = useState([
         {
             Title: 'The Avengers',
@@ -33,15 +50,52 @@ const Index = () => {
             <SafeAreaView />
             <YStack padding={20} gap={20}>
                 <H1>Movies</H1>
+                <XGroup>
+                    <Input
+                        flex={1}
+                        placeholder={'Search'}
+                        value={search}
+                        onChangeText={setSearch}
+                    />
+                    <Button icon={<Ionicons name={'search'} size={24} />} />
+                </XGroup>
+
+                <XGroup>
+                    <Button
+                        icon={<Ionicons name={'chevron-back'} size={24} />}
+                        onPress={() => {
+                            if (page > 1) setPage(page - 1)
+                        }}
+                    />
+                    <Input
+                        flex={1}
+                        placeholder={'1'}
+                        placeholderTextColor={'$color'}
+                        fontSize={20}
+                        textAlign="center"
+                        keyboardType="numeric"
+                        inputMode="numeric"
+                        value={page.toString()}
+                    />
+                    <Button
+                        icon={<Ionicons name={'chevron-forward'} size={24} />}
+                        onPress={() => setPage(page + 1)}
+                    />
+                </XGroup>
+
                 <YGroup
                     separator={
                         <Separator width={'100%'} borderColor={'$color10'} />
                     }
                     marginBottom={100}
                 >
-                    {movies.map((movie, index) => (
-                        <ContentTile key={index} content={movie} />
-                    ))}
+                    {loading && (
+                        <Spinner size="large" scale={1.5} color={'$color10'} />
+                    )}
+                    {!loading &&
+                        movies.map((movie, index) => (
+                            <ContentTile key={index} content={movie} />
+                        ))}
                 </YGroup>
             </YStack>
         </ScrollView>
--- app/(tabs)/(series)/index.tsx
+++ app/(tabs)/(series)/index.tsx
@@ -1,9 +1,26 @@
 import ContentTile from '@/components/ContentTile'
+import { Ionicons } from '@expo/vector-icons'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { H1, ScrollView, Separator, Text, View, YGroup, YStack } from 'tamagui'
+import {
+    Button,
+    H1,
+    Input,
+    ScrollView,
+    Separator,
+    Spinner,
+    Text,
+    View,
+    XGroup,
+    XStack,
+    YGroup,
+    YStack,
+} from 'tamagui'
 
 const Index = () => {
+    const [search, setSearch] = useState('')
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
     const [series, setSeries] = useState([
         {
             Title: "The Avengers: Earth's Mightiest Heroes",
@@ -39,15 +56,50 @@ const Index = () => {
             <SafeAreaView />
             <YStack padding={20} gap={20}>
                 <H1>Series</H1>
+                <XGroup>
+                    <Input
+                        flex={1}
+                        placeholder={'Search'}
+                        value={search}
+                        onChangeText={setSearch}
+                    />
+                    <Button icon={<Ionicons name={'search'} size={24} />} />
+                </XGroup>
+                <XGroup>
+                    <Button
+                        icon={<Ionicons name={'chevron-back'} size={24} />}
+                        onPress={() => {
+                            if (page > 1) setPage(page - 1)
+                        }}
+                    />
+                    <Input
+                        flex={1}
+                        placeholder={'1'}
+                        placeholderTextColor={'$color'}
+                        fontSize={20}
+                        textAlign="center"
+                        keyboardType="numeric"
+                        inputMode="numeric"
+                        value={page.toString()}
+                    />
+                    <Button
+                        icon={<Ionicons name={'chevron-forward'} size={24} />}
+                        onPress={() => setPage(page + 1)}
+                    />
+                </XGroup>
                 <YGroup
                     separator={
                         <Separator width={'100%'} borderColor={'$color10'} />
                     }
                     marginBottom={100}
                 >
-                    {series.map((series, index) => (
-                        <ContentTile key={index} content={series} />
-                    ))}
+                    {loading && (
+                        <Spinner size="large" scale={1.5} color={'$color10'} />
+                    )}
+                    {!loading &&
+                        series.map((series, index) => (
+                            <ContentTile key={index} content={series} />
+                        ))}
                 </YGroup>
             </YStack>
         </ScrollView>
