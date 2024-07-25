832078eb1fb9596d2340d8e304851e96a2efeb81
tanle-dev <lenhuttan.programing@gmail.com>
2024-07-25 10:57:10 -0400

    series screen search and pagination

--- app/(tabs)/(series)/index.tsx
+++ app/(tabs)/(series)/index.tsx
@@ -1,5 +1,5 @@
 import ContentTile from '@/components/ContentTile'
-import { Ionicons } from '@expo/vector-icons'
+import { Feather, Ionicons } from '@expo/vector-icons'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {
@@ -12,28 +12,47 @@ import {
     Text,
     XGroup,
     YGroup,
-    YStack,
+    YStack
 } from 'tamagui'
 
 const Index = () => {
     const [search, setSearch] = useState('')
     const [page, setPage] = useState(1)
+
+    const [status, setStatus] = useState<'idle' | 'loading' | 'success' | 'error'>('idle')
+    const [totalResults, setTotalResults] = useState(0)
+
     const [loading, setLoading] = useState(false)
     const [series, setSeries] = useState([])
     const [error, setError] = useState('')
 
-    const seriesAPI = async () => {
+    const seriesAPI = async (newSearch = false) => {
+        if (newSearch) {
+            setPage(1)
+        }
+
         const baseURL = `https://www.omdbapi.com/?s=${search}&type=series&page=${page}&apikey=c2266d16`
-        setError('')
         setLoading(true)
+        setStatus('loading')
+
         const response = await fetch(baseURL)
         const data = await response.json()
 
         if (response.ok) {
-            setSeries(data.Search)
+            if (data.Response === 'True') {
+                setStatus('success')
+                setTotalResults(Math.ceil(data.totalResults / 10))
+                setSeries(data.Search)
+            }
+            else {
+                setError(data.Error)
+                setSeries([])
+                setStatus('error')
+            }
         } else {
             setError('Sorry, an error occurred. Please try again later.')
             setSeries([])
+            setStatus('error')
         }
         setLoading(false)
     }
@@ -56,15 +75,25 @@ const Index = () => {
                     />
                     <Button
                         icon={<Ionicons name={'search'} size={24} />}
-                        onPress={seriesAPI}
+                        onPress={() => seriesAPI(true)}
                     />
                 </XGroup>
+
+                {status === 'success' && series.length > 0 && (
                 <XGroup>
+                    <Button
+                            icon={<Feather name="chevrons-left" size={24} />}
+                            onPress={() => {
+                                if (page > 1) setPage(1)
+                            }}
+                            disabled={page <= 1}
+                    />
                     <Button
                         icon={<Ionicons name={'chevron-back'} size={24} />}
                         onPress={() => {
                             if (page > 1) setPage(page - 1)
                         }}
+                        disabled={page <= 1}
                     />
                     <Input
                         flex={1}
@@ -74,30 +103,118 @@ const Index = () => {
                         textAlign="center"
                         keyboardType="numeric"
                         inputMode="numeric"
-                        value={page.toString()}
+                        value={`${page.toString()}/${totalResults}`}
                     />
                     <Button
                         icon={<Ionicons name={'chevron-forward'} size={24} />}
                         onPress={() => setPage(page + 1)}
+                        disabled={page >= totalResults}
+                    />
+                    <Button
+                            icon={<Feather name="chevrons-right" size={24} />}
+                            onPress={() => setPage(totalResults)}
+                            disabled={page >= totalResults}
                     />
                 </XGroup>
+                )}
+                
+
+
                 <YGroup
                     separator={
                         <Separator width={'100%'} borderColor={'$color10'} />
                     }
-                    marginBottom={100}
                 >
-                    {loading && (
+                    {status == "loading" && (
                         <Spinner size="large" scale={1.5} color={'$color10'} />
                     )}
-                    {error && <Text>{error}</Text>}
-                    {!loading &&
-                        !error &&
-                        series &&
+
+                    {status === 'idle' && (
+                        <Text
+                            color={'$color7'}
+                            fontSize={12}
+                            textAlign={'center'}
+                        >
+                            Nothing to show. Please search for a series.
+                        </Text>
+                    )}
+
+                    {status == 'error' && search === '' && (
+                        <Text
+                        color={'$color7'}
+                        fontSize={12}
+                        textAlign={'center'}
+                    >
+                        Please enter a search term.
+                    </Text>
+                    )}
+
+                    {status == 'error' && search !== '' && (
+                        <Text
+                        color={'$color7'}
+                        fontSize={12}
+                        textAlign={'center'}
+                    >
+                        Sorry, an error occurred. Please try again.
+                    </Text>
+                    )}
+
+                    {status === 'success' && series.length === 0 && (
+                        <Text
+                            color={'$color7'}
+                            fontSize={12}
+                            textAlign={'center'}
+                        >
+                            No series found. Please try a different search.
+                        </Text>
+                    )}
+
+
+                    {status === "success" &&
+                        series.length > 0 &&
                         series.map((series, index) => (
                             <ContentTile key={index} content={series} />
-                        ))}
+                    ))}
                 </YGroup>
+
+                {status === 'success' && series.length > 0 && (
+                <XGroup>
+                    <Button
+                            icon={<Feather name="chevrons-left" size={24} />}
+                            onPress={() => {
+                                if (page > 1) setPage(1)
+                            }}
+                            disabled={page <= 1}
+                    />
+                    <Button
+                        icon={<Ionicons name={'chevron-back'} size={24} />}
+                        onPress={() => {
+                            if (page > 1) setPage(page - 1)
+                        }}
+                        disabled={page <= 1}
+                    />
+                    <Input
+                        flex={1}
+                        placeholder={'1'}
+                        placeholderTextColor={'$color'}
+                        fontSize={20}
+                        textAlign="center"
+                        keyboardType="numeric"
+                        inputMode="numeric"
+                        value={`${page.toString()}/${totalResults}`}
+                    />
+                    <Button
+                        icon={<Ionicons name={'chevron-forward'} size={24} />}
+                        onPress={() => setPage(page + 1)}
+                        disabled={page >= totalResults}
+                    />
+                    <Button
+                            icon={<Feather name="chevrons-right" size={24} />}
+                            onPress={() => setPage(totalResults)}
+                            disabled={page >= totalResults}
+                    />
+                </XGroup>
+                )}
             </YStack>
         </ScrollView>
     )
