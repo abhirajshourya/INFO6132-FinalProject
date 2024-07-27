11513fc40e9e16cb4c5f65136a1395eae0d0185c
DannyGlade <darshannariya12@gmail.com>
2024-07-27 13:01:35 -0400

    chore: Update firebase config

--- database/config.tsx
+++ database/config.tsx
@@ -4,13 +4,13 @@ import { getReactNativePersistence, initializeAuth } from 'firebase/auth'
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHISb4hG9kTxfYvCNqKmcjSG6zIJV9A9w",
-    authDomain: "info6132-b79b2.firebaseapp.com",
-    projectId: "info6132-b79b2",
-    storageBucket: "info6132-b79b2.appspot.com",
-    messagingSenderId: "728824690448",
-    appId: "1:728824690448:web:fec9c7a774b7e274669795"
-};
+    apiKey: 'AIzaSyCzduablzPPbKkQiv-H71jLeOf9JZT7WeM',
+    authDomain: 'info6132-finalproject-1d51c.firebaseapp.com',
+    projectId: 'info6132-finalproject-1d51c',
+    storageBucket: 'info6132-finalproject-1d51c.appspot.com',
+    messagingSenderId: '276619363556',
+    appId: '1:276619363556:web:8982d687dd1b08b35618f1',
+}
 
 const app = initializeApp(firebaseConfig)
 export const auth = initializeAuth(app, {
